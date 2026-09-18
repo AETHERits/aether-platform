9c48b3b4a9ee9a06f2f8b68455e8d6bbcc5746dd
Achraf Bourja <abourja@studenti.apuliadigital.it>
2026-09-18 08:55:10 +0200

    feat(frontend): form registrazione incidente [HEL-501]

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'incidenti/nuovo',
+    pathMatch: 'full'
+  },
+  {
+    path: 'incidenti/nuovo',
+    loadComponent: () =>
+      import('./incidenti/form-incidenti/incidenti-form').then(m => m.IncidentiForm)
+  }
+];
