d8745339dd568b21eb3179ce6a408047a0b52068
abourja-10 <abourja@studenti.apuliadigital.it>
2026-09-21 13:32:03 +0200

    [HEL-501] Frontend form registrazione incidente (#73)
    * feat: scaffold Angular frontend
    
    * feat: scaffold Angular frontend and Spring Boot backend base
    
    * feat(frontend): form registrazione incidente [HEL-501]
    
    ---------
    
    Co-authored-by: Simoncino0 <simostella06@gmail.com>

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,17 @@
+
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'incidenti/nuovo',
+    pathMatch: 'full'
+  },
+  {
+    path: 'incidenti/nuovo',
+    loadComponent: () =>
+      import('./incidenti/form-incidenti/incidenti-form').then((m) => m.IncidentiForm)
+  },
   {
     path: 'missions/new',
     loadComponent: () =>
